70ba1fb960a98f2a18aaebb75750757c9584653d
isarth <isarth@me.com>
2017-08-05 14:36:13 +0530

    readme

--- dcgan.py
+++ dcgan.py
@@ -33,7 +33,7 @@ data=np.array(ds)
 
 X=data[:,1:]
 
-
+X=(X/255.0)
 # In[51]:
 
 X=np.reshape(X,(X.shape[0],28,28,1))
@@ -43,7 +43,7 @@ X=np.reshape(X,(X.shape[0],28,28,1))
 
 D=Sequential()
 depth=64
-dropout=0.4
+dropout=0.3
 D.add(Conv2D(64,5,strides=2,input_shape=(28,28,1),padding='same'))
 D.add(LeakyReLU(alpha=0.2))
 D.add(Dropout(dropout))
@@ -65,7 +65,7 @@ D.summary()
 # In[53]:
 
 G=Sequential()
-dropout=0.4
+dropout=0.3
 G.add(Dense(12544,input_shape=(100,)))
 G.add(BatchNormalization(momentum=0.9))
 G.add(Activation('relu'))
@@ -89,13 +89,13 @@ G.summary()
 
 # In[54]:
 
-optimizer =RMSprop(lr=0.002, clipvalue=1.0, decay=6e-8)
+optimizer =Adam(lr=0.001, decay=1e-4)
 D.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
-
+D.trainable=False
 
 # In[55]:
 
-optimizer_G = RMSprop(lr=0.001, clipvalue=1.0, decay=3e-8)
+optimizer_G = Adam(lr=0.0005, decay=1e-4)
 AM=Sequential()
 AM.add(G)
 AM.add(D)
@@ -112,20 +112,12 @@ for epoch in range(5000):
     x=np.concatenate((X[rand_no:rand_no+256],images_fake))
     y=np.ones([512,1])
     y[256:,:]=0
+    D.trainable=True
     D.train_on_batch(x, y)
+    D.trainable=False
     y = np.ones([256, 1])
     noise = np.random.uniform(-1.0, 1.0, size=[256, 100])
     a_loss=AM.train_on_batch(noise,y)
-    if (epoch==5000):
-        noise=np.random.uniform(-1,1,size=[100,100])
-        images_fake=G.predict(noise)
-        for ix in range(100):
-            test=np.reshape(images_fake[ix],(28,28))
-            #plt.imshow(test,cmap='gray')
-            #plt.axis('off')
-            #plt.show()
-            name='/output/'+str(ix)+'e.png'
-            plt.imsave(name,test,cmap='gray')
 
 
 t2=datetime.now()
@@ -134,7 +126,7 @@ t2=datetime.now()
 # In[57]:
 
 #from keras.models import load_model
-#G.save('/output/G.h5')
+G.save('/output/G.h5')
 #D.save('/output/D.h5')
 
 
